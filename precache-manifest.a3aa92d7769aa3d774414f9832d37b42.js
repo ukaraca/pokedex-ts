e1bc953ad69b4362b860b45c0f6fb7de0a3a6190
Umutcan Karaca <umutcan.karaca@prodabilisim.com>
2020-09-25 06:08:25 +0300

    Updates

---
+++ precache-manifest.a3aa92d7769aa3d774414f9832d37b42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe81c443b9923b3e91734040c405990",
+    "url": "/ukaraca/pokedex-ts/index.html"
+  },
+  {
+    "revision": "d51511f52e8212a4bc8d",
+    "url": "/ukaraca/pokedex-ts/static/css/2.87d4210f.chunk.css"
+  },
+  {
+    "revision": "d51511f52e8212a4bc8d",
+    "url": "/ukaraca/pokedex-ts/static/js/2.da88f510.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/ukaraca/pokedex-ts/static/js/2.da88f510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f596439db6a391757ecb",
+    "url": "/ukaraca/pokedex-ts/static/js/main.f5243fda.chunk.js"
+  },
+  {
+    "revision": "88bfd41b64bf8325de6a",
+    "url": "/ukaraca/pokedex-ts/static/js/runtime-main.9949828f.js"
+  }
+]);
